ce84eaa7cafd23da2f675b5a498caf6bcf64d6c1
Luis <denkfeld@outlook.de>
2026-05-10 10:40:58 +0200

    Site-URL korrigiert auf luis-magnolia.netlify.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://magnolia.netlify.app',
+  site: 'https://luis-magnolia.netlify.app',
   integrations: [sitemap()],
 });
